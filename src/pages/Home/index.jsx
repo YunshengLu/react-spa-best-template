3ac0fa0b0df002d9e52ebc9bbcd786e7e5eacb84
YunshengLu <934201509@qq.com>
2022-06-16 15:37:15 +0800

    'SPA模板'

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+// import {} from './style'
+
+const Home = () => {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home
+
